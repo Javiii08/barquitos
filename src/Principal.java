c421971a4f5106636ae8b3b533f4670650b75903
Javiii08 <javiviperez1@gmail.com>
2024-01-25 12:52:14 +0100

    Comentario javadoc: nombre del programa y autor

--- src/Principal.java
+++ src/Principal.java
@@ -1,5 +1,10 @@
+/**
+ * Barquitos
+ * @author Francisco Javier Pérez López
+ */
 public class Principal {
     public static void main(String[] args) {
+
         System.out.println("Barquitos 0.8.1");
     }
 }
